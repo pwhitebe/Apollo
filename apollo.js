4ae21496d244882efa9cb965ccb0547efdaf4931
pwhitebe <pwhitebe@gmail.com>
2014-03-10 15:30:07 -0400

    Added a second test and a dependency on neo4j

--- apollo.js
+++ apollo.js
@@ -3,11 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 var express = require('express'),
     app = express();
     
 require('./lib/reverser')(app);
 
+
+
 app.listen(8080);
+  console.log('Express server listening on port 8080');
